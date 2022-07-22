e4a6137874deb914303904f13776c7f066f8fa33
oceancx <oceancx@gmail.com>
2022-07-22 23:57:29 +0800

    init

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,36 @@
+using ImGuiNET;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        ImGuiUn.Layout += OnLayout;
+    }
+
+    void OnDisable()
+    {
+        ImGuiUn.Layout -= OnLayout;
+    }
+
+    void OnLayout()
+    {
+        ImGui.ShowDemoWindow();
+    }
+}
+
